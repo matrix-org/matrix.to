e6dcf6b3f5b9818039b5b56b8b9c7ac01a713ac2
Jorik Schellekens <joriksch@gmail.com>
2020-08-03 18:40:09 +0100

    Add copyright, remove whitespace

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,11 +1,25 @@
+/*
+Copyright 2020 The Matrix.org Foundation C.I.C.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 import React from 'react';
 import { addDecorator } from '@storybook/react';
 import { withA11y } from '@storybook/addon-a11y';
 import { withKnobs } from '@storybook/addon-knobs';
 import { withDesign } from 'storybook-addon-designs'
 import { addParameters } from '@storybook/react';
-
-
 
 // Default styles
 import "../src/index.scss";
@@ -26,3 +40,4 @@ addParameters({
         {name: 'white', value: '#FFFFFF', default: true},
     ],
 });
+
