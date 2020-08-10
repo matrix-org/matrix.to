5b2c36833ce834466cc93e734551bba091e05beb
Jorik Schellekens <joriksch@gmail.com>
2020-08-10 12:35:15 +0100

    Remove federtaed argument

--- src/parser/parser.ts
+++ src/parser/parser.ts
@@ -1,12 +1,97 @@
 import forEach from "lodash/forEach";
 
 import {
-  LinkDiscriminator,
-  SafeLink,
-  Link,
-  LinkContent,
-  Arguments,
+    LinkKind,
+    SafeLink,
+    Link,
+    LinkContent,
+    Arguments,
+    Permalink,
 } from "./types";
+
+/*
+ * Verifiers are regexes which will match valid
+ * identifiers to their type. (This is a lie, they
+ * can return anything)
+ */
+type Verifier<A> = [RegExp, A];
+export const roomVerifiers: Verifier<LinkKind.Alias | LinkKind.RoomId>[] = [
+    [/^#([^/:]+?):(.+)$/, LinkKind.Alias],
+    [/^!([^/:]+?):(.+)$/, LinkKind.RoomId],
+];
+export const verifiers: Verifier<LinkKind>[] = [
+    [/^[!#]([^/:]+?):(.+?)\/\$([^/:]+?):(.+?)$/, LinkKind.Permalink],
+    [/^@([^/:]+?):(.+)$/, LinkKind.UserId],
+    [/^\+([^/:]+?):(.+)$/, LinkKind.GroupId],
+    ...roomVerifiers,
+];
+
+/*
+ * identifyTypeFromRegex applies the verifiers to the identifier and
+ * returns the identifier's type
+ */
+export function identifyTypeFromRegex<T, F>(
+    identifier: string,
+    verifiers: Verifier<T>[],
+    fail: F
+): T | F {
+    if (identifier !== encodeURI(identifier)) {
+        return fail;
+    }
+
+    return verifiers.reduce<T | F>((kind, verifier) => {
+        if (kind !== fail) {
+            return kind;
+        }
+
+        if (identifier.match(verifier[0])) {
+            return verifier[1];
+        }
+
+        return kind;
+    }, fail);
+}
+
+/*
+ * Parses a permalink.
+ * Assumes the permalink is correct.
+ */
+export function parsePermalink(identifier: string): Permalink {
+    const [roomLink, eventId] = identifier.split("/");
+    const roomKind = identifyTypeFromRegex(
+        roomLink,
+        roomVerifiers,
+        // This is hacky but we're assuming identifier is a valid permalink
+        LinkKind.Alias
+    );
+
+    return {
+        roomKind,
+        roomLink,
+        eventId,
+    };
+}
+
+/*
+ * Repalces null with undefined
+ */
+function bottomExchange<T>(nullable: T | null): T | undefined {
+    if (nullable === null) return undefined;
+    return nullable;
+}
+
+/*
+ * parseArgs parses the <extra args> part of matrix.to links
+ */
+export function parseArgs(args: string): Arguments {
+    const params = new URLSearchParams(args);
+
+    return {
+        vias: params.getAll("via"),
+        client: bottomExchange(params.get("client")),
+        sharer: bottomExchange(params.get("sharer")),
+    };
+}
 
 /*
  * parseLink takes a striped matrix.to hash link (without the '#/' prefix)
@@ -14,80 +99,36 @@ import {
  * be ParseFailed
  */
 export function parseHash(hash: string): Link {
-  const [identifier, args] = hash.split("?");
+    const [identifier, args] = hash.split("?");
 
-  const kind = identifyTypeFromRegex(
-    identifier,
-    verifiers,
-    LinkDiscriminator.ParseFailed
-  );
+    const kind = identifyTypeFromRegex(
+        identifier,
+        verifiers,
+        LinkKind.ParseFailed
+    );
 
-  let parsedLink: LinkContent = {
-    identifier,
-    arguments: parseArgs(args),
-    originalLink: hash,
-  };
+    const parsedLink: LinkContent = {
+        identifier,
+        arguments: parseArgs(args),
+        originalLink: hash,
+    };
 
-  if (kind === LinkDiscriminator.Permalink) {
-    const { roomKind, roomLink, eventId } = parsePermalink(identifier);
+    if (kind === LinkKind.Permalink) {
+        const { roomKind, roomLink, eventId } = parsePermalink(identifier);
+
+        return {
+            kind,
+            ...parsedLink,
+            roomKind,
+            roomLink,
+            eventId,
+        };
+    }
 
     return {
-      kind,
-      ...parsedLink,
-      roomKind,
-      roomLink,
-      eventId,
+        kind,
+        ...parsedLink,
     };
-  }
-
-  return {
-    kind,
-    ...parsedLink,
-  };
-}
-
-/*
- * Parses a permalink.
- * Assumes the permalink is correct.
- */
-export function parsePermalink(identifier: string) {
-  const [roomLink, eventId] = identifier.split("/");
-  const roomKind = identifyTypeFromRegex(
-    roomLink,
-    roomVerifiers,
-    // This is hacky but we're assuming identifier is a valid permalink
-    LinkDiscriminator.Alias
-  );
-
-  return {
-    roomKind,
-    roomLink,
-    eventId,
-  };
-}
-
-/*
- * parseArgs parses the <extra args> part of matrix.to links
- */
-export function parseArgs(args: string): Arguments {
-  const params = new URLSearchParams(args);
-  const _federated = params.get("federated");
-  const federated = _federated !== null ? _federated === "true" : null;
-
-  return {
-    vias: params.getAll("via"),
-    federated: bottomExchange(federated),
-    client: bottomExchange(params.get("client")),
-    sharer: bottomExchange(params.get("sharer")),
-  };
-}
-
-/*
- * Repalces null with undefined
- */
-function bottomExchange<T>(nullable: T | null): T | undefined {
-  if (nullable === null) return undefined;
-  return nullable;
 }
 
 /*
@@ -96,72 +137,27 @@ function bottomExchange<T>(nullable: T | null): T | undefined {
  * This is handy function in case the Link was constructed.
  */
 export function toURL(origin: string, link: SafeLink): URL {
-  switch (link.kind) {
-    case LinkDiscriminator.GroupId:
-    case LinkDiscriminator.UserId:
-    case LinkDiscriminator.RoomId:
-    case LinkDiscriminator.Alias:
-    case LinkDiscriminator.Permalink:
-      const params = new URLSearchParams();
-      forEach(link.arguments, (value, key) => {
-        if (value === undefined) {
-          // do nothing
-        } else if (key === "vias") {
-          (<string[]>(<unknown>value)).forEach((via) =>
-            params.append("via", via)
-          );
-        } else {
-          params.append(key, value.toString());
-        }
-      });
+    const params = new URLSearchParams();
+    const url = new URL(origin);
+    switch (link.kind) {
+        case LinkKind.GroupId:
+        case LinkKind.UserId:
+        case LinkKind.RoomId:
+        case LinkKind.Alias:
+        case LinkKind.Permalink:
+            forEach(link.arguments, (value, key) => {
+                if (value === undefined) {
+                    // do nothing
+                } else if (key === "vias") {
+                    (value as string[]).forEach((via) =>
+                        params.append("via", via)
+                    );
+                } else {
+                    params.append(key, value.toString());
+                }
+            });
 
-      const url = new URL(origin);
-      url.hash = `/${link.identifier}?${params.toString()}`;
-      return url;
-  }
-}
-
-/*
- * Verifiers are regexes which will match valid
- * identifiers to their type. (This is a lie, they
- * can return anything)
- */
-type Verifier<A> = [RegExp, A];
-export const roomVerifiers: Verifier<
-  LinkDiscriminator.Alias | LinkDiscriminator.RoomId
->[] = [
-  [/^#([^\/:]+?):(.+)$/, LinkDiscriminator.Alias],
-  [/^!([^\/:]+?):(.+)$/, LinkDiscriminator.RoomId],
-];
-export const verifiers: Verifier<LinkDiscriminator>[] = [
-  [/^[\!#]([^\/:]+?):(.+?)\/\$([^\/:]+?):(.+?)$/, LinkDiscriminator.Permalink],
-  [/^@([^\/:]+?):(.+)$/, LinkDiscriminator.UserId],
-  [/^\+([^\/:]+?):(.+)$/, LinkDiscriminator.GroupId],
-  ...roomVerifiers,
-];
-
-/*
- * identifyTypeFromRegex applies the verifiers to the identifier and
- * returns the identifier's type
- */
-export function identifyTypeFromRegex<T, F>(
-  identifier: string,
-  verifiers: Verifier<T>[],
-  fail: F
-): T | F {
-  if (identifier !== encodeURI(identifier)) {
-    return fail;
-  }
-
-  return verifiers.reduce<T | F>((discriminator, verifier) => {
-    if (discriminator !== fail) {
-      return discriminator;
+            url.hash = `/${link.identifier}?${params.toString()}`;
     }
-
-    if (identifier.match(verifier[0])) {
-      return verifier[1];
-    }
-
-    return discriminator;
-  }, fail);
+    return url;
 }
--- src/parser/types.ts
+++ src/parser/types.ts
@@ -1,56 +1,52 @@
 export interface Arguments {
-  vias: string[];
-  // Schemeless http identifier
-  client?: string;
-  // Indicates whether a room exists on a federating server (assumed to be the
-  // default), or if the client must connect via the server identified by the
-  // room ID or event ID
-  federated?: boolean;
-  // MXID
-  sharer?: string;
+    vias: string[];
+    // Schemeless http identifier
+    client?: string;
+    // MXID
+    sharer?: string;
 }
 
 export interface LinkContent {
-  identifier: string;
-  arguments: Arguments;
-  originalLink: string;
+    identifier: string;
+    arguments: Arguments;
+    originalLink: string;
 }
 
-export enum LinkDiscriminator {
-  Alias = "ALIAS",
-  RoomId = "ROOM_ID",
-  UserId = "USER_ID",
-  Permalink = "PERMALINK",
-  GroupId = "GROUP_ID",
-  ParseFailed = "PARSE_FAILED",
+export enum LinkKind {
+    Alias = "ALIAS",
+    RoomId = "ROOM_ID",
+    UserId = "USER_ID",
+    Permalink = "PERMALINK",
+    GroupId = "GROUP_ID",
+    ParseFailed = "PARSE_FAILED",
 }
 
 export interface Alias extends LinkContent {
-  kind: LinkDiscriminator.Alias;
+    kind: LinkKind.Alias;
 }
 
 export interface RoomId extends LinkContent {
-  kind: LinkDiscriminator.RoomId;
+    kind: LinkKind.RoomId;
 }
 
 export interface UserId extends LinkContent {
-  kind: LinkDiscriminator.UserId;
+    kind: LinkKind.UserId;
 }
 
 export interface GroupId extends LinkContent {
-  kind: LinkDiscriminator.GroupId;
+    kind: LinkKind.GroupId;
 }
 
 export interface Permalink extends LinkContent {
-  kind: LinkDiscriminator.Permalink;
-  roomKind: LinkDiscriminator.RoomId | LinkDiscriminator.Alias;
-  roomLink: string;
-  eventId: string;
+    kind: LinkKind.Permalink;
+    roomKind: LinkKind.RoomId | LinkKind.Alias;
+    roomLink: string;
+    eventId: string;
 }
 
 export interface ParseFailed {
-  kind: LinkDiscriminator.ParseFailed;
-  originalLink: string;
+    kind: LinkKind.ParseFailed;
+    originalLink: string;
 }
 
 export type SafeLink = Alias | RoomId | UserId | Permalink | GroupId;
