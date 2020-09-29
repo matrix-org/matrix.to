2eed84fc23f7316a9c71c3510ab600600cea4c90
J. Ryan Stinnett <jryans@gmail.com>
2020-09-29 16:41:28 +0100

    Rename Element client file

--- src/clients/index.ts
+++ src/clients/index.ts
@@ -16,7 +16,7 @@ limitations under the License.
 
 import { Client } from './types';
 
-import Element, { ElementDevelop } from './Element.io';
+import Element, { ElementDevelop } from './Element';
 import Weechat from './Weechat';
 import Nheko from './Nheko';
 import Fractal from './Fractal';
