fda01e0bc4e9f2d011b9bee8e2f521d362560ad0
Bruno Windels <brunow@element.io>
2020-12-03 11:07:06 +0100

    header, cleanup

--- src/main.js
+++ src/main.js
@@ -1,3 +1,19 @@
+/*
+Copyright 2020 The Matrix.org Foundation C.I.C.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 import {xhrRequest} from "./utils/xhr.js";
 import {RootViewModel} from "./RootViewModel.js";
 import {RootView} from "./RootView.js";
@@ -16,7 +32,7 @@ export async function main(container) {
 	window.__rootvm = vm;
 	const view = new RootView(vm);
 	container.appendChild(view.mount());
-	window.addEventListener('hashchange', event => {
+	window.addEventListener('hashchange', () => {
 		vm.updateHash(location.hash);
 	});
 }
