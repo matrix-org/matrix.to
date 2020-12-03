2dc3ad70a37464288f58bad61d20a0ce2b4a2c2e
Bruno Windels <brunow@element.io>
2020-12-03 11:06:25 +0100

    editor and lint settings

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaVersion": 2020,
+        "sourceType": "module"
+    },
+    "rules": {
+        "no-console": "off"
+    }
+};
