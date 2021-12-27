a1bb51ad2d35336e0d8be438e87ee7020a69bdf8
Carl Schwan <carl@carlschwan.eu>
2021-12-27 10:51:06 +0100

    Update src/open/clients/Quaternion.js
    Co-authored-by: Alexey Rusakov <Kitsune-Ral@users.sf.net>

--- src/open/clients/Quaternion.js
+++ src/open/clients/Quaternion.js
@@ -20,7 +20,7 @@ export class Quaternion {
     get id() { return "quaternion"; }
     get name() { return "Quaternion"; }
     get icon() { return "images/client-icons/quaternion.svg"; }
-    get author() { return "Felix Rohrbach and Alexey Rusakov "; }
+    get author() { return "The Quotient project"; }
     get homepage() { return "https://github.com/quotient-im/Quaternion"; }
     get platforms() { return [Platform.Windows, Platform.macOS, Platform.Linux]; }
     get description() { return 'Qt5 and C++ cross-platform desktop Matrix client.'; }
