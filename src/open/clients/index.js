8d8c48346828dccab6d9a25b90e8ddf4da501e0a
Felix Stupp <felix.stupp@banananet.work>
2023-02-13 22:32:45 +0100

    Add SchildiChat to clients
    mostly adapted version of element.js,
    as SchildiChat is an fork of Element.

--- src/open/clients/index.js
+++ src/open/clients/index.js
@@ -14,20 +14,22 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-import {Element} from "./Element.js";
-import {Weechat} from "./Weechat.js";
-import {Nheko} from "./Nheko.js";
-import {Fractal} from "./Fractal.js";
-import {Quaternion} from "./Quaternion.js";
-import {Tensor} from "./Tensor.js";
-import {Fluffychat} from "./Fluffychat.js";
-import {NeoChat} from "./NeoChat.js";
-import {Syphon} from "./Syphon.js";
-import {Thunderbird} from "./Thunderbird.js";
+import { Element } from "./Element.js";
+import { SchildiChat } from "./SchildiChat.js";
+import { Weechat } from "./Weechat.js";
+import { Nheko } from "./Nheko.js";
+import { Fractal } from "./Fractal.js";
+import { Quaternion } from "./Quaternion.js";
+import { Tensor } from "./Tensor.js";
+import { Fluffychat } from "./Fluffychat.js";
+import { NeoChat } from "./NeoChat.js";
+import { Syphon } from "./Syphon.js";
+import { Thunderbird } from "./Thunderbird.js";
 
 export function createClients() {
     return [
         new Element(),
+        new SchildiChat(),
         new Weechat(),
         new Nheko(),
         new Fractal(),
