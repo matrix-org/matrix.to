0b92cd5fd882f5040fab5a9fa14750f91661ea9b
Half-Shot <will@half-shot.uk>
2026-07-21 12:52:07 +0100

    Update eslint

---
+++ eslint.config.js
@@ -0,0 +1,35 @@
+import js from "@eslint/js";
+import globals from "globals";
+
+export default [
+    {
+        ignores: ["build/**"],
+    },
+    js.configs.recommended,
+    {
+        files: ["src/**/*.js"],
+        languageOptions: {
+            ecmaVersion: 2020,
+            sourceType: "module",
+            globals: globals.browser,
+        },
+        rules: {
+            "no-console": "off",
+            // many client implementations share an interface and don't use every argument
+            "no-unused-vars": ["error", {args: "none"}],
+            "getter-return": ["error", {allowImplicit: true}],
+            "no-empty": ["error", {allowEmptyCatch: true}],
+        },
+    },
+    {
+        files: ["scripts/**/*.js"],
+        languageOptions: {
+            ecmaVersion: 2020,
+            sourceType: "module",
+            globals: globals.node,
+        },
+        rules: {
+            "no-console": "off",
+        },
+    },
+];
